586fc7f6fd72d48900407ba9bb1c6ea4cd6a75c0
Y HARI VARMA <135501111+harivarma44@users.noreply.github.com>
2026-01-16 09:55:40 +0530

    Create Get-IP.ps1

---
+++ Get-IP.ps1
@@ -0,0 +1,6 @@
+git init
+git add Get-IP.ps1
+git commit -m "Add IP check script"
+git branch -M main
+git remote add origin https://github.com/<your-username>/ip-check-script.git
+git push -u origin main
